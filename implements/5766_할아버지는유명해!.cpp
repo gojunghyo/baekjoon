083d97fed3648e747a2160c15164243d54c138db
gojghoMZ <100115805+gojghoMZ@users.noreply.github.com>
2023-03-24 08:38:48 +0900

    구현

---
+++ implements/5766_할아버지는유명해!.cpp
@@ -0,0 +1,57 @@
+//
+// Created by MZ01-GOJGHO on 2023/03/23.
+//
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<int,int> mp;
+
+int n,m;
+
+int main() {
+
+    while(true){
+        cin >> n >> m;
+
+        if(n == 0 && m == 0) break;
+        int fiMax = -1e9;
+        int seMax = -1e9;
+        mp.clear();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                int value;
+                cin >> value;
+                mp.insert({value, mp[value]++});
+                if(fiMax < mp[value]) {
+                    fiMax = mp[value];
+                }
+            }
+        }
+
+        for(auto i : mp){
+            if(i.second == fiMax){
+                mp[i.first] = 0;
+            }
+        }
+
+        for(auto i : mp) {
+           if(seMax < i.second){
+               seMax = i.second;
+           }
+        }
+
+        for(auto i : mp){
+            if(i.second == seMax){
+                cout << i.first << ' ';
+            }
+        }
+
+
+        cout << '\n';
+
+    }
+
+
+}
